0fa5cec632a9f796af3026a4210a452127c912b2
jivii <67268647+jivii@users.noreply.github.com>
2020-06-30 12:18:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,29 +14,73 @@ app.engine('html', require('ejs').renderFile);
 
 app.use(express.static(path.join(__dirname, './views')));
 
+app.use(session({
+  secret: uuidv4(),
+  resave: false,
+  saveUninitialized: true,
+}))
+
+// Use JSON parser for all non-webhook routes
+app.use((req, res, next) => {
+  if (req.originalUrl === "/webhook") {
+    next();
+  } else {
+    bodyParser.json()(req, res, next);
+  }
+});
+
+
 // Future Code Goes Here
 app.get('/', (req, res) => {
   res.render('index');
   //res.send('An alligator approaches!');
 });
-  
-  app.post("/createAccount", (req, res) => {
-   
-		stripe.accounts.create({
-			type: 'custom',
-			country: 'US',
-			email: 'jenny.rosen@example.com',
-			requested_capabilities: [
-			  'card_payments',
-			  'transfers',
-			]
-		  }).then(function (account) {
-					console.log(JSON.stringify(account, null, 2));
-				})
 
-				.then(() => res.render("completed.html"))
-				.catch(err => console.log(err));
-  });
+app.get("/get-oauth-link", async (req, res) => {
+  const state = uuidv4();
+  req.session.state = state
+  const args = new URLSearchParams({state, client_id: process.env.STRIPE_CLIENT_ID})
+  const url = `https://connect.stripe.com/express/oauth/authorize?${args.toString()}`;
+  return res.send({url});
+});
+
+app.get("/authorize-oauth", async (req, res) => {
+  const { code, state } = req.query;
+
+  // Assert the state matches the state you provided in the OAuth link (optional).
+  if(req.session.state !== state) {
+    return res.status(403).json({ error: 'Incorrect state parameter: ' + state });
+  }
+
+  // Send the authorization code to Stripe's API.
+  stripe.oauth.token({
+    grant_type: 'authorization_code',
+    code
+  }).then(
+    (response) => {
+      var connected_account_id = response.stripe_user_id;
+      saveAccountId(connected_account_id);
+
+      // Render some HTML or redirect to a different page.
+      return res.redirect(301, '/completed.ejs')
+    },
+    (err) => {
+      if (err.type === 'StripeInvalidGrantError') {
+        return res.status(400).json({error: 'Invalid authorization code: ' + code});
+      } else {
+        return res.status(500).json({error: 'An unknown error occurred.'});
+      }
+    }
+  );
+});
+
+const saveAccountId = (id) => {
+  // Save the connected account ID from the response to your database.
+  console.log('Connected account ID: ' + id);
+}
+
+  
+
 
 
 const port = process.env.PORT || 3000;
